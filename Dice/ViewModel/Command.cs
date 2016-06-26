7a8f1ca858b9bfccc35aca1c4a35bbdca3b66843
Krzysztof Szczech <krzysztof.szczech@hotmail.com>
2016-06-27 00:26:30 +0200

    Init

---
+++ Dice/ViewModel/Command.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Dice.ViewModel
+{
+    internal class Command : ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute != null ? _canExecute(parameter) : true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_execute != null)
+            {
+                _execute(parameter);
+            }
+        }
+
+        public void RiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ Dice/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Dice.ViewModel
+{
+    internal class MainPageViewModel
+    {
+        public Command NewGameCommand { get; set; }
+        public Command LoadGameCommand { get; set; }
+        public Command SaveGameCommand { get; set; }
+        public Command CheckDiceStateCommand { get; set; }
+        public Command AboutApplicationCommand { get; set; }
+        public Command CloseApplicationCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            NewGameCommand = new Command(NewGameExecuteMethod);
+            LoadGameCommand = new Command(LoadGameExecuteMethod);
+            SaveGameCommand = new Command(SaveGameExecuteMethod);
+            CheckDiceStateCommand = new Command(CheckDiceStateExecuteMethod);
+            AboutApplicationCommand = new Command(AboutApplicationExecuteMethod);
+            CloseApplicationCommand = new Command(CloseApplicationExecuteMethod);
+        }
+
+        private void NewGameExecuteMethod(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadGameExecuteMethod(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveGameExecuteMethod(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CheckDiceStateExecuteMethod(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AboutApplicationExecuteMethod(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CloseApplicationExecuteMethod(object o)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
